ecd9f794c518874eaf60710536b8f028a66d5340
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-21 23:34:16 +0200

    add h1 tag

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -16,6 +16,7 @@ const Home: React.FC = () => {
     <div
       className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
+      <h1>Airbnb Application Clone system</h1>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
