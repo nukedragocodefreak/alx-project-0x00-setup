d24d811bdec3e8c114ec864b083723dc7a5cf700
BetNoelM <noelm@betsoftware.com>
2025-09-20 16:33:20 +0200

    add

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,8 +1,19 @@
+import { PillProps } from "@/interfaces";
+
+
 const Pill: React.FC = () => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
       <p className=" text-sm ">Title</p>
+
+      <div className="p-2 flex gap-2 mt-2">
+        <Pill title="Top Villa" />
+        <Pill title="Self CheckIn" />
+        <Pill title="Free Reschedule" />
+ </div>
     </div>
+
+    
   )
 }
 
