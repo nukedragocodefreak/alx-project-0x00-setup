4636c0a646465a7d48ed3f1c330b6ce6f830f7c7
BetNoelM <noelm@betsoftware.com>
2025-09-20 16:31:38 +0200

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Card from "@/components/Card"
 
 const Landing: React.FC = () => {
   return (
     <div>
-      <h1 className="text-xl font-extralight">Landing Page</h1>
+      <h1 className="text-xl font-extralight"> <Card /> Landing Page</h1>
     </div>
   );
 };
