a7a5ab76700ecc0a6df99b4f8909d5b52cc8203d
BetNoelM <noelm@betsoftware.com>
2025-09-20 16:39:30 +0200

    add new changes

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Button from "@/components/Button";
 import Card from "@/components/Card"
 
 const Landing: React.FC = () => {
   return (
-    <div>
+     <div>
       <h1 className="text-xl font-extralight"> <Card /> Landing Page</h1>
+
+      {/* Buttons */}
+      <div className="flex flex-wrap gap-4">
+        {/* Small */}
+        <Button title="Small Rounded" styles="rounded-sm text-sm" />
+        
+        {/* Medium */}
+        <Button title="Medium Rounded" styles="rounded-md text-base" />
+        
+        {/* Large */}
+        <Button title="Large Rounded" styles="rounded-full text-lg" />
+      </div>
     </div>
   );
 };
