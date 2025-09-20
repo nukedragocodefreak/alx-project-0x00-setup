aab3d47f893fbe8aed6fab51e511e19de115c524
BetNoelM <noelm@betsoftware.com>
2025-09-20 16:42:31 +0200

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,18 +6,6 @@ const Landing: React.FC = () => {
   return (
      <div>
       <h1 className="text-xl font-extralight"> <Card /> Landing Page</h1>
-
-      {/* Buttons */}
-      <div className="flex flex-wrap gap-4">
-        {/* Small */}
-        <Button title="Small Rounded" styles="rounded-sm text-sm" />
-        
-        {/* Medium */}
-        <Button title="Medium Rounded" styles="rounded-md text-base" />
-        
-        {/* Large */}
-        <Button title="Large Rounded" styles="rounded-full text-lg" />
-      </div>
     </div>
   );
 };
